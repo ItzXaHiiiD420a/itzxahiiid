5445012e236eff4e429055b184bea100261ec1df
ItzXaHiiiD420a <96961482+ItzXaHiiiD420a@users.noreply.github.com>
2022-01-02 05:41:18 +0600

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,8 @@
+import base64, codecs
+magic = 'cHJpbnQoIiIiCuKWkeKWiOKWiOKWiOKWiOKWiOKVl+KWkeKWiOKWiOKVl+KWkeKWkeKWiOKWiOKVl+KWkeKWiOKWiOKWiOKWiOKWiOKVl+KWkeKWkeKWiOKWiOKWiOKWiOKWiOKWiOKVl+KWiOKWiOKVl+KWkeKWkeKWiOKWiOKVlwrilojilojilZTilZDilZDilojilojilZfilojilojilZHilpHilojilojilZTilZ3ilojiloj'
+love = 'vyMGvyMQvyMQvybwvybwvyMsvybwvybwvyMGvyMQvyMQvyMQvyMQvyM3vybwvybwvyMUvycUvycUvybwvybwvyMRX4cnV4cnV4cnV4cnV4cnV4cnV4cnV4cJE4cnV4cnV4cnV4cnV4cnV4cJD4cJq4cnE4cnV4cnV4cnV4cnV4cnV4cnV4cnV4cJE4cJn4cnV4cnV4cnV4cnV4cnV4cJK4cnE4cnV4cnV4cnV4cnV4cnV4cnV4cnV4c'
+god = 'WRCuKWiOKWiOKVlOKVkOKVkOKWiOKWiOKVkeKWiOKWiOKVlOKVkOKWiOKWiOKVl+KWkeKWiOKWiOKVlOKVkOKVkOKWiOKWiOKVkeKWkeKVmuKVkOKVkOKVkOKWiOKWiOKVl+KWiOKWiOKVlOKVkOKVkOKWiOKWiOKVkQrilojilojilZHilpHilpHilojilojilZHilojilojilZHilpHilZrilojilojilZfilojilojilZHilpHil'
+destiny = 'cUvybwvybwvyMUvybwvybwvybwvybwvybwvybwvyMGvyM3vybwvybwvyMUvycUvycUvybwvybwvyMRX4cJn4cJD4cJq4cnE4cnE4cJn4cJD4cJq4cJn4cJD4cJq4cnE4cnE4cJn4cJD4cJq4cJn4cJD4cJq4cnE4cnE4cJn4cJD4cJq4cJn4cJD4cJD4cJD4cJD4cJD4cJq4cnE4cJn4cJD4cJq4cnE4cnE4cJn4cJD4cJqVvVvXD=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
